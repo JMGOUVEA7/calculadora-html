b4c2fa4a4b4f8791f99d40d5d39b00c0d60ff821
José Miguel <you@example.com>
2026-05-27 14:57:13 -0300

    adicionados comentarios de explicacao da funcao no js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@ function insertToDisplay(data) {
     document.querySelector('#display').value += data
 }
 
+//função limpar "C"
 function clean() {
     document.querySelector('#display').value = ''
 }
 
+// função apagar "<-"
 function back() {
     const display = document.querySelector('#display')
     display.value = display.value.slice(0, -1)
 }
 
+// função resultado "="
 function result() {
     const display = document.querySelector('#display')
     try {
